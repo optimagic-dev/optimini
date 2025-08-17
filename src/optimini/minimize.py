39e46c8bc8fec1c9202bfc3136438b9da985ccb8
Janos Gabler <janos.gabler@gmail.com>
2025-08-17 15:57:08 +0200

    Add algorithm classes.

--- src/optimini/minimize.py
+++ src/optimini/minimize.py
@@ -1,5 +1,4 @@
-from scipy.optimize import minimize as scipy_minimize
-
+from optimini.algorithms import OPTIMIZER_REGISTRY
 from optimini.converter import Converter
 from optimini.history import History
 from optimini.internal_problem import InternalProblem
@@ -7,18 +6,13 @@ from optimini.utils import OptimizeResult
 
 
 def minimize(fun, params, method, options=None):
-    """Minimize a function using a given method"""
     options = {} if options is None else options
     converter = Converter(params)
     history = History()
-    internal_fun = InternalProblem(fun, converter, history)
+    problem = InternalProblem(fun, converter, history)
     x0 = converter.flatten(params)
-    raw_res = scipy_minimize(
-        fun=internal_fun.fun,
-        x0=x0,
-        method=method,
-        options=options,
-    )
+    algo = OPTIMIZER_REGISTRY[method](**options)
+    raw_res = algo._solve_internal_problem(problem, x0)
     res = OptimizeResult(
         x=converter.unflatten(raw_res.x),
         history=history,
