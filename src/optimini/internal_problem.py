7aec01e709f0f2a1ffd19a9d0fdc2a2b80f06afb
Janos Gabler <janos.gabler@gmail.com>
2025-08-17 15:24:33 +0200

    Add history collection.

--- src/optimini/internal_problem.py
+++ src/optimini/internal_problem.py
@@ -1,10 +1,13 @@
 class InternalProblem:
     """Wraps a user provided function to add functionality"""
 
-    def __init__(self, fun, converter):
+    def __init__(self, fun, converter, history):
         self._user_fun = fun
         self._converter = converter
+        self._history = history
 
     def fun(self, x):
         params = self._converter.unflatten(x)
-        return self._user_fun(params)
+        value = self._user_fun(params)
+        self._history.add(value, params)
+        return value
--- src/optimini/minimize.py
+++ src/optimini/minimize.py
@@ -1,16 +1,17 @@
-from copy import deepcopy
-
 from scipy.optimize import minimize as scipy_minimize
 
 from optimini.converter import Converter
+from optimini.history import History
 from optimini.internal_problem import InternalProblem
+from optimini.utils import OptimizeResult
 
 
 def minimize(fun, params, method, options=None):
     """Minimize a function using a given method"""
     options = {} if options is None else options
     converter = Converter(params)
-    internal_fun = InternalProblem(fun, converter)
+    history = History()
+    internal_fun = InternalProblem(fun, converter, history)
     x0 = converter.flatten(params)
     raw_res = scipy_minimize(
         fun=internal_fun.fun,
@@ -18,6 +19,9 @@ def minimize(fun, params, method, options=None):
         method=method,
         options=options,
     )
-    res = deepcopy(raw_res)
-    res.x = converter.unflatten(res.x)
+    res = OptimizeResult(
+        x=converter.unflatten(raw_res.x),
+        history=history,
+        fun=raw_res.fun,
+    )
     return res
